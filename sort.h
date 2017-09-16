0331fbb461b20cc58ccb121c1f4c8355a994f779
Tanya Kryukova <ttkryukova@gmail.com>
2017-09-16 23:44:58 +0000

    added prototype for swap_dlist() for insertion sort

--- sort.h
+++ sort.h
@@ -19,6 +19,7 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void swap_dlist(listint_t **head, listint_t *c_node, listint_t *n_node);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
