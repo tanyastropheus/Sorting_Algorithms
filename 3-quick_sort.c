971008880ea8d077567f7295d264911090b25959
Tanya Kryukova <ttkryukova@gmail.com>
2017-09-19 05:04:00 +0000

    producing output expected with Lomuto partition, but output does not match example output

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -27,66 +27,61 @@ void swap(int *array, int idx_1, int idx_2)
  * Return: index of the pivot element in the array
  */
 
-unsigned int partition_lomuto(int *array, int start_idx, int end_idx)
+unsigned int partition_lomuto(int *array, unsigned int start_idx, unsigned int end_idx)
 {
-	unsigned int i, parti_idx, pivot;
+	unsigned int i, parti_idx;
 
-	pivot = array[end_idx];
-	i = 0;
+	i = start_idx;
+	parti_idx = start_idx;
 	while (i < end_idx)  /* loop through elem in the array excluding pivot */
 	{
-		if (array[i] <= pivot)  /* if elem is less/equal to pivot */
+		if (array[i] <= array[end_idx])  /* if elem is less/equal to pivot */
 		{
-			if (parti_idx != i)  /* swap only when elems are different */
-				swap(array, array[parti_idx], array[i]);
+			if (parti_idx != i)  /* swap only when it's not the same elem */
+				swap(array, parti_idx, i);
 
 			parti_idx++;  /* tracking the divide position btw less & more elems */
 		}
 		i++;
 	}
-	swap(array, array[p_idx], pivot);  /* swap pivot with the divide */
+	swap(array, parti_idx, end_idx);  /* swap pivot with the divide */
 
 	return (parti_idx);
 }
 
 /**
- * selection_sort - sorts an array of integers in ascending order using
- * selection sort
+ * quick_sort_recur - sorts an array of integers in ascending order using
+ * the Quick sort algorithm with Lomuto partition scheme
  * @array: pointer to an array of integers
  * @size: size of the array
  * Return: void
  */
 
-/* Is it possible to optimize unnecessary looping when sorting is done? */
-void selection_sort(int *array, size_t size)
+void quick_sort_recur(int *array, unsigned int start_idx, unsigned int end_idx, size_t size)
 {
-	int i, j;
-	unsigned int idx;
-	int temp;
-	short flag;
+	unsigned int parti_idx;
 
-	i = -1;
-	while (i < (int)size - 1)  /* times loop through the array; (n - 1) */
-	{
-		flag = 0;
-		j = i + 1;  /* only loop through unsorted elem */
-		temp = array[j];  /* holding value for comparison */
-		while (j < (int)size - 1)
-		{
-			if (temp > array[j + 1])  /* compare against each subsequent elem */
-			{
-				temp = array[j + 1];
-				idx = j + 1;  /* holding index of the smaller elem */
-				flag = 1;
-			}
-			j++;
-		}
-		if (flag == 1)  /* min value out of unsorted elem is found */
-		{
-			swap_array(array, i + 1, idx);
-			print_array(array, size);
-		}
-		i++;
-	}
+	if (size == 1)
+		return;
+	parti_idx = partition_lomuto(array, start_idx, end_idx);
+	print_array(array, size);
+	if (parti_idx - 1 > start_idx && parti_idx - 1 <= end_idx)
+		quick_sort_recur(array, start_idx, parti_idx - 1, size);
+	if (parti_idx + 1 < end_idx)
+		quick_sort_recur(array, parti_idx + 1, end_idx, size);
 
+}
+
+
+/**
+ * quick_sort - sorts an array of integers in ascending order using
+ * the Quick sort algorithm with Lomuto partition scheme
+ * @array: pointer to an array of integers
+ * @size: size of the array
+ * Return: void
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	quick_sort_recur(array, 0, size - 1, size);
 }
