e00312386d58852c105cfdce0fbbae8138458e98
Tanya Kryukova <ttkryukova@gmail.com>
2017-09-20 04:23:04 +0000

    moving files

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,4 +1,29 @@
 #include "sort.h"
+/**
+ * swap_dlist - swap two nodes in a doubly linked list
+ * @head: double pointer to the head of the doubly linked list
+ * @c_node: pointer to the current node to be swapped
+ * @n_node: pointer to the next ndoe to be swapped
+ * Return: void
+ */
+
+void swap_dlist(listint_t **head, listint_t *c_node, listint_t *n_node)
+{
+	if (!c_node->prev)  /* if swapping the head node */
+		*head = c_node->next;
+	else
+		c_node->prev->next = n_node;
+
+	if (n_node->next)
+		n_node->next->prev = c_node;
+
+	n_node->prev = c_node->prev;
+	c_node->next = n_node->next;
+	c_node->prev = n_node;
+	n_node->next = c_node;
+
+}
+
 
 /**
  * insertion_sort_list - sorts a doubly linked list of integers in ascending
@@ -9,30 +34,30 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	int temp;
-	int counter;
-	int i;
+	listint_t *backward;
 	listint_t *c_node;
 
+	if (!list || !*list)
+		return;
+
 	c_node = *list;
-	while (c_node->next) /* check each element in the array */
+	while (c_node->next) /* if array size >= 2 */
 	{
-		if (c_node->n > c_node->next->n)  /* compare with the next element */
+		if (c_node->n > c_node->next->n)  /* compare with next elem */
 		{
-			temp = c_node->n;
-			c_node->n = c_node->next->n;
-			c_node->next->n = temp;
+			/* c_node ptr automatically advances to the next after swap */
+			swap_dlist(list, c_node, c_node->next);
+			print_list(*list);
+			backward = c_node->prev;
+
+			/* compare with all prev elems; if out of order, then swap */
+			while (backward->prev && backward->n < backward->prev->n)
+			{
+				swap_dlist(list, backward->prev, backward);
+				print_list(*list);
+			}
 		}
-		counter =0;
-		while (c_node->prev && c_node->n < c_node->prev->n)  /* after swapping, compare with all the previous elemtns; if out of order, than swap */
-		{
-			temp = c_node->n;
-			c_node->n = c_node->prev->n;
-			c_node->prev->n = temp;
-			c_node = c_node->prev;
-			counter++;
-		}
-		for (i = 0; i <= counter; i++)
+		else
 			c_node = c_node->next;
 	}
 }
--- sort.h
+++ sort.h
@@ -19,8 +19,13 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void swap_dlist(listint_t **head, listint_t *c_node, listint_t *n_node);
 void insertion_sort_list(listint_t **list);
+void swap_array(int *array, int idx_1, int idx_2);
 void selection_sort(int *array, size_t size);
+void swap(int *array, int idx_1, int idx_2);
+unsigned int partition_lomuto(int *array, unsigned int start_idx, unsigned int end_idx, size_t size);
+void quick_sort_recur(int *array, unsigned int start_idx, unsigned int end_idx, size_t size);
 void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
